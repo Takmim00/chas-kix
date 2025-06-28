045228657da0867c7a231452125f9f01150cae24
Takmim00 <takmimm@gmail.com>
2025-06-28 21:27:41 +0600

    opportunities section added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import SearchBid from './SearchBid';
+import Opportunities from './Opportunities';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
             <SearchBid/>
+            <Opportunities/>
         </div>
     );
 };
---
+++ src/components/Opportunities.jsx
@@ -0,0 +1,35 @@
+const Opportunities = () => {
+  return (
+    <div className="bg-[#848239]">
+      <div className="grid grid-cols-2 w-11/12 mx-auto py-20">
+        <div>
+          <h1 className="text-6xl font-bold text-[#DDE4B8]">
+            OPPORTUNITIES <br />
+            WITHOUT <br />
+            MIDDLEMEN
+          </h1>
+          <p className="text-[#DDE4B8] text-2xl font-normal pt-6">
+            WE’RE HERETO HELPYOU GROW
+          </p>
+        </div>
+        <div className="text-[#F5F1EB] space-y-3 ">
+          <p className="border-b text-2xl py-2">
+            Keep more of what you earn with transparent fees.
+          </p>
+          <p className="border-b text-2xl py-2">
+            Access AI insights to price, pitch, and scale smarter.
+          </p>
+          <p className="border-b text-2xl py-2">
+            Get seen by buyers actively searching for your services.
+          </p>
+          <p className="border-b text-2xl py-2">
+            Join a community built to support solopreneurs like you.
+          </p>
+          <p className=" text-xl py-2">JOINTHE SERVICE COMMUNITY →</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Opportunities;
